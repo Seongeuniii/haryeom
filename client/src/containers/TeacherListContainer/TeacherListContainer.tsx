6023c5af78da04f95cb47c21656732e59c9d451a
김성은 <kse8538@naver.com>
2024-01-17 03:26:12 +0900

    Add : 🌱 라우팅 페이지들 생성

---
+++ client/src/containers/TeacherListContainer/TeacherListContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeacherListContainer = () => {
+    return <div>TeacherListContainer</div>;
+};
+
+export const getServerSideProps = () => {
+    console.log('teacher');
+};
+
+export default TeacherListContainer;
