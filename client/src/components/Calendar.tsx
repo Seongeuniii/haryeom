118da21adcccd40647d8cb6fd51b69cfd73fcce9
김성은 <kse8538@naver.com>
2024-01-20 11:53:41 +0900

    Feat: ✨ react-calendar 커스텀, 캘린더 컴포넌트 (#S10P12A807-138)

---
+++ client/src/components/Calendar.tsx
@@ -0,0 +1,70 @@
+import Calendar, { OnArgs } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import styled from 'styled-components';
+
+interface MyCalendarProps {
+    selectedDate: Date;
+    handleDayClick: (date: Date) => void;
+    handleYearMonthChange: (onArgs: OnArgs) => void;
+}
+
+const MyCalendar = ({ selectedDate, handleDayClick, handleYearMonthChange }: MyCalendarProps) => {
+    return (
+        <CalendarContainer>
+            <Calendar
+                locale="en-US"
+                value={selectedDate}
+                onClickDay={handleDayClick}
+                onActiveStartDateChange={handleYearMonthChange}
+            ></Calendar>
+        </CalendarContainer>
+    );
+};
+
+const CalendarContainer = styled.div`
+    padding: 15px;
+
+    .react-calendar {
+        border: none;
+        width: 100%;
+        background-color: transparent;
+    }
+
+    .react-calendar abbr[title='Sunday'],
+    .react-calendar abbr[title='Monday'],
+    .react-calendar abbr[title='Tuesday'],
+    .react-calendar abbr[title='Wednesday'],
+    .react-calendar abbr[title='Thursday'],
+    .react-calendar abbr[title='Friday'],
+    .react-calendar abbr[title='Saturday'] {
+        text-decoration: none;
+        border-bottom: none;
+        &:after {
+            content: none;
+        }
+    }
+
+    .react-calendar__tile--active,
+    .react-calendar__tile--hasActive {
+        background: ${({ theme }) => theme.PRIMARY_LIGHT} !important;
+    }
+
+    .react-calendar__month-view__days {
+        button {
+            border-radius: 100%;
+        }
+        .react-calendar__tile {
+            height: 3.5em;
+        }
+
+        .react-calendar__tile--now {
+            background: ${({ theme }) => theme.PRIMARY} !important;
+        }
+
+        .react-calendar__tile--now.react-calendar__tile--active {
+            background: ${({ theme }) => theme.PRIMARY} !important;
+        }
+    }
+`;
+
+export default MyCalendar;
