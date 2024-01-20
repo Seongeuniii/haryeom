e5183dfa23fd56149111a97d67eb7302fe4e9ecb
이상영 <syxxng25@gmail.com>
2024-01-20 14:06:12 +0900

    Chore : 🧹 더미 데이터 변경 (#S10P12A807-132)
    정합성을 위한 데이터 변경

--- server/src/main/resources/db/data.sql
+++ server/src/main/resources/db/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO member (id, role, created_at, updated_at) VALUES
-(1, 'STUDENT', NOW(), NOW()),
-(2, 'TEACHER', DATEADD('SECOND', 20, CURRENT_TIMESTAMP()), DATEADD('SECOND', 10, CURRENT_TIMESTAMP()));
+(1, 'TEACHER', NOW(), NOW()),
+(2, 'STUDENT', DATEADD('SECOND', 20, CURRENT_TIMESTAMP()), DATEADD('SECOND', 10, CURRENT_TIMESTAMP()));
 
 INSERT INTO tutoring (id, status, created_at, updated_at) VALUES
 (1, 'IN_PROGRESS', NOW(), NOW()),
@@ -8,7 +8,7 @@ INSERT INTO tutoring (id, status, created_at, updated_at) VALUES
 
 INSERT INTO resources (id, member_id, resource_name, total_page, created_at, updated_at) VALUES
 (1, 1, 'Resource 1', 120, NOW(), NOW()),
-(2, 2, 'Resource 2', 200, DATEADD('SECOND', 20, CURRENT_TIMESTAMP()), DATEADD('SECOND', 10, CURRENT_TIMESTAMP()));
+(2, 1, 'Resource 2', 200, DATEADD('SECOND', 20, CURRENT_TIMESTAMP()), DATEADD('SECOND', 10, CURRENT_TIMESTAMP()));
 
 INSERT INTO homework (id, resource_id, tutoring_id, deadline, start_page, end_page, status, is_deleted, created_at, updated_at) VALUES
 (1, 1, 1, '2023-12-31', 10, 20, 'UNCONFIRMED', false, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
