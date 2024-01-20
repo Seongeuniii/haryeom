a1854ddab7d5e8a4127fe09e5ef37dfe84f282bc
김성은 <kse8538@naver.com>
2024-01-20 14:05:20 +0900

    Style: 💄 style theme - background 추가

--- client/src/theme/styled.d.ts
+++ client/src/theme/styled.d.ts
@@ -4,6 +4,7 @@ declare module 'styled-components' {
     export interface DefaultTheme {
         PRIMARY: string;
         PRIMARY_LIGHT: string;
+        BACKGROUND: string;
         SECONDARY: string;
         BORDER_LIGHT: string;
         WHITE: string;
