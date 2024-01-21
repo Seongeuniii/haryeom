6b6bb2e0da9c0ad06b703ae02df090c8595c3af8
김성은 <kse8538@naver.com>
2024-01-22 07:06:53 +0900

    Feat: ✨ 헤더 로고 추가, 사용자별  네비게이션 객체 생성

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { IUserRole } from '@/apis/user/user';
 
 export interface Page {
     name: string;
     link: string;
 }
 
-interface HeaderProps {
-    navLinks: Page[];
-}
+const navLinks: { [key in IUserRole]: Page[] } = {
+    teacher: [
+        {
+            name: '마이홈',
+            link: '/',
+        },
+        {
+            name: '교재관리',
+            link: '/review',
+        },
+    ],
+    student: [
+        {
+            name: '마이홈',
+            link: '/',
+        },
+        {
+            name: '복습하렴',
+            link: '/review',
+        },
+    ],
+    guest: [],
+};
 
-const Header = ({ navLinks }: HeaderProps) => {
+const testUserRole = 'student';
+
+const Header = () => {
     const handleClickUser = () => {};
 
     return (
         <StyledHeader>
             <HeaderWrapper>
                 <Nav>
-                    <Link href="/">
-                        <Logo>하렴</Logo>
+                    <Link href="/find">
+                        <Logo src="/images/logo.png" alt="logo" />
                     </Link>
-                    {navLinks.map((page, index) => (
+                    {navLinks[testUserRole].map((page, index) => (
                         <Link href={page.link} key={`nav_${index}`}>
                             {page.name}
                         </Link>
@@ -42,26 +65,27 @@ const StyledHeader = styled.header`
     align-items: center;
     justify-content: center;
     border-bottom: 1px solid ${({ theme }) => theme.BORDER_LIGHT};
-    background-color: ${({ theme }) => theme.WHITE};
     background-color: ${({ theme }) => theme.BACKGROUND};
 `;
 
 const HeaderWrapper = styled.div`
     width: 90%;
-    max-width: 1200px;
+    max-width: 1300px;
     display: flex;
     align-items: center;
     justify-content: space-between;
 `;
 
-const Logo = styled.div`
-    font-size: 1.6em;
+const Logo = styled.img`
+    width: 80px;
+    height: 40px;
+    margin-right: 1em;
 `;
 
 const Nav = styled.nav`
     display: flex;
     align-items: center;
-    gap: 2em;
+    gap: 1.6em;
 `;
 
 const User = styled.button``;
