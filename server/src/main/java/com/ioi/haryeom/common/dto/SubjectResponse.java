5c37b9525b6ad49e21586fdc71ec9997ba891e00
이상영 <syxxng25@gmail.com>
2024-01-23 15:06:16 +0900

    Fix : 🐞 SubjectResponse 충돌 해결 (#S10P12A807-151)

--- server/src/main/java/com/ioi/haryeom/common/dto/SubjectResponse.java
+++ server/src/main/java/com/ioi/haryeom/common/dto/SubjectResponse.java
@@ -1,15 +1,17 @@
 package com.ioi.haryeom.common.dto;
 
+import com.ioi.haryeom.common.domain.Subject;
 import lombok.Getter;
 
 @Getter
 public class SubjectResponse {
 
     private Long subjectId;
-    private String subjectName;
 
-    public SubjectResponse(Long subjectId, String subjectName) {
-        this.subjectId = subjectId;
-        this.subjectName = subjectName;
+    private String name;
+
+    public SubjectResponse(Subject subject) {
+        this.subjectId = subject.getId();
+        this.name = subject.getName();
     }
 }
