dfaed381f212c4b04e93b9c3b317533961c9302a
이상영 <syxxng25@gmail.com>
2024-01-26 14:39:09 +0900

    Feat : ✨ 채팅방 연결시 매칭 요청 조회 구현  (#S10P12A807-154)

--- server/src/main/java/com/ioi/haryeom/matching/dto/CreateMatchingResponse.java
+++ server/src/main/java/com/ioi/haryeom/matching/dto/CreateMatchingResponse.java
@@ -1,31 +1,41 @@
 package com.ioi.haryeom.matching.dto;
 
 import com.ioi.haryeom.chat.domain.ChatRoom;
+import com.ioi.haryeom.common.domain.Subject;
+import com.ioi.haryeom.common.dto.SubjectResponse;
+import com.ioi.haryeom.member.domain.Member;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class CreateMatchingResponse {
 
+    private String matchingId;
+    private Long chatRoomId;
     private Long receiveMemberId;
     private String senderName;
-    private String subjectName;
+    private SubjectResponse subject;
     private Integer hourlyRate;
 
-    private CreateMatchingResponse(Long receiveMemberId, String senderName, String subjectName, Integer hourlyRate) {
+    private CreateMatchingResponse(String matchingId, Long chatRoomId, Long receiveMemberId, String senderName,
+        Subject subject, Integer hourlyRate) {
+        this.matchingId = matchingId;
+        this.chatRoomId = chatRoomId;
         this.receiveMemberId = receiveMemberId;
         this.senderName = senderName;
-        this.subjectName = subjectName;
+        this.subject = new SubjectResponse(subject);
         this.hourlyRate = hourlyRate;
     }
 
-    public static CreateMatchingResponse of(ChatRoom chatRoom, String subjectName, Integer hourlyRate) {
+    public static CreateMatchingResponse of(String matchingId, ChatRoom chatRoom, Member member, Subject subject,
+        Integer hourlyRate) {
         return new CreateMatchingResponse(
-            chatRoom.getTeacherMember().getId(),
-            chatRoom.getStudentMember().getName(),
-            subjectName,
+            matchingId,
+            chatRoom.getId(),
+            chatRoom.getOppositeMember(member).getId(),
+            member.getName(),
+            subject,
             hourlyRate
         );
     }
+
 }
--- server/src/main/java/com/ioi/haryeom/matching/manager/MatchingManager.java
+++ server/src/main/java/com/ioi/haryeom/matching/manager/MatchingManager.java
@@ -1,6 +1,6 @@
 package com.ioi.haryeom.matching.manager;
 
-import com.ioi.haryeom.matching.dto.CreateMatchingRequest;
+import com.ioi.haryeom.matching.dto.CreateMatchingResponse;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Component;
@@ -8,18 +8,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class MatchingManager {
 
-    private final Map<String, CreateMatchingRequest> matchingMap = new ConcurrentHashMap<>();
+    private final Map<String, CreateMatchingResponse> matchingMap = new ConcurrentHashMap<>();
+    private final Map<Long, String> chatRoomMatchingMap = new ConcurrentHashMap<>();
 
-    public void addMatching(String matchingId, CreateMatchingRequest matchingRequest) {
-        matchingMap.put(matchingId, matchingRequest);
+    public void addTutoringMatching(String matchingId, CreateMatchingResponse matchingResponse) {
+        matchingMap.put(matchingId, matchingResponse);
+        chatRoomMatchingMap.put(matchingResponse.getChatRoomId(), matchingId);
     }
 
-    public CreateMatchingRequest getMatchingRequestByMatchingId(String matchingId) {
+    public CreateMatchingResponse getTutoringMatchingRequestByMatchingId(String matchingId) {
         return matchingMap.get(matchingId);
     }
 
-    public void removeMatchingRequestByMatchingId(String matchingId) {
+    public void removeTutoringMatchingRequestByMatchingId(String matchingId) {
+        chatRoomMatchingMap.remove(matchingMap.get(matchingId).getChatRoomId());
         matchingMap.remove(matchingId);
+    }
+
+    public CreateMatchingResponse getTutoringMatchingResponseByChatRoomId(Long chatRoomId) {
+        String matchingId = chatRoomMatchingMap.get(chatRoomId);
+        return matchingMap.get(matchingId);
+    }
+
+    public boolean existMatchingResponseByChatRoomId(Long chatRoomId) {
+        return chatRoomMatchingMap.containsKey(chatRoomId);
     }
 
 }
