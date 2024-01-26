93ee76c3b7cffbf292b388c61c384fea1f9fbf4c
이태호 <leetag1163@naver.com>
2024-01-26 13:39:18 +0900

    Feat: ✨ Security 필터 AccessDenied 예외 처리 구현 (#S10P12A807-182)

--- server/src/main/java/com/ioi/haryeom/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/ioi/haryeom/config/JwtAuthenticationFilter.java
@@ -3,11 +3,15 @@ package com.ioi.haryeom.config;
 
 import static com.ioi.haryeom.auth.type.ErrorCode.EMPTY_TOKEN;
 import static com.ioi.haryeom.auth.type.ErrorCode.INVALID_TOKEN;
+import static com.ioi.haryeom.auth.type.ErrorCode.NOT_FOUND_MEMBER;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import com.ioi.haryeom.advice.exception.BusinessException;
-import com.ioi.haryeom.auth.exception.TokenNotFoundException;
+import com.ioi.haryeom.advice.exception.NotFoundException;
+import com.ioi.haryeom.auth.dto.AccessTokenResponse;
+import com.ioi.haryeom.auth.exception.FilterException;
 import com.ioi.haryeom.auth.service.TokenService;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
@@ -19,10 +23,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
@@ -33,23 +39,36 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
         throws IOException, ServletException {
         try {
             String token = tokenService.resolveToken((HttpServletRequest) request);
-
-            if (tokenService.validateToken(token)) { // access_token 유효할 때
+            if (tokenService.validateToken(token)) { // accessToken 유효할 때
                 Authentication authentication = tokenService.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-            } else { // access_token 유효하지 않을 때 재발급
-                String accessToken =
-                    tokenService.reissueAccessToken((HttpServletRequest) request, (HttpServletResponse) response);
-                ((HttpServletResponse) response).setHeader("Authorization", accessToken);
-                Authentication authentication = tokenService.getAuthentication(accessToken);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else { // accessToken이 유효하지 않을 때 예외 던짐  (access_token 유효하지 않을 때 재발급)
+                throw new FilterException(INVALID_TOKEN, UNAUTHORIZED);
+
+//                String accessToken =
+//                    tokenService.reissueAccessToken((HttpServletRequest) request,
+//                        (HttpServletResponse) response);
+//                Cookie에 바로 설정
+//                Cookie accessCookie = new Cookie("accesToken", accessToken);
+//                accessCookie.setHttpOnly(true);
+//                accessCookie.setPath("/");
+//                ((HttpServletResponse) response).addCookie(accessCookie);
+
+//               Response Body에 담아서 보냄
+//                response.getWriter()
+//                    .print(AccessTokenResponse.builder().accessToken(accessToken).build());
+
+//                Authentication authentication = tokenService.getAuthentication(accessToken);
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
 
-        }catch (TokenNotFoundException e){
-            request.setAttribute("errorCode", INVALID_TOKEN);
-            request.setAttribute("httpStatus", UNAUTHORIZED);
-        }
-        catch (SignatureException | MalformedJwtException e) {
+        } catch (FilterException e) {
+            request.setAttribute("errorCode", e.getMessage());
+            request.setAttribute("httpStatus", e.getHttpStatus());
+        } catch (NotFoundException e) {
+            request.setAttribute("errorCode", NOT_FOUND_MEMBER);
+            request.setAttribute("httpStatus", NOT_FOUND);
+        } catch (SignatureException | MalformedJwtException e) {
             request.setAttribute("errorCode", INVALID_TOKEN);
             request.setAttribute("httpStatus", UNAUTHORIZED);
         } catch (IllegalArgumentException e) {
