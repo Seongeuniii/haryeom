a3465da42fb7039b37fae48e663b84ff070e95dd
김태윤 <whddn_0426@naver.com>
2024-01-30 09:16:15 +0900

    Hotfix : 🚑️ WebMvcConfig AllowedOrigin 변경

--- server/src/main/java/com/ioi/haryeom/config/WebMvcConfig.java
+++ server/src/main/java/com/ioi/haryeom/config/WebMvcConfig.java
@@ -12,8 +12,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedOrigins("http://localhost:3000", "https://i10a807.p.ssafy.io:3000",
-                "http://i10a807.p.ssafy.io:3000")
+            .allowedOrigins("http://localhost:3000", "https://localhost:3000",
+                "https://i10a807.p.ssafy.io:3000",
+                "http://i10a807.p.ssafy.io:3000", "http://localhost:8080", "https://localhost:8080",
+                "http://i10a807.p.ssafy.io:8080", "https://i10a807.p.ssafy.io:8080")
             .allowedHeaders("*")
             .allowCredentials(true)
             .exposedHeaders(HttpHeaders.LOCATION);
