08c502ab7a7f519e7b41dd56a7175c4a4d4b3aa9
허준혁 <demise1426@naver.com>
2024-01-31 17:22:54 +0900

    Hotfix : 🚑️ 과외 일정 요청 유효성 검사 수정(#S10P12A807-268)

--- server/src/main/java/com/ioi/haryeom/tutoring/dto/TutoringScheduleRequest.java
+++ server/src/main/java/com/ioi/haryeom/tutoring/dto/TutoringScheduleRequest.java
@@ -8,16 +8,17 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
 public class TutoringScheduleRequest {
 
-    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "과외 일자의 형식은 yyyy-MM-dd 여야 합니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotNull(message = "과외 일자는 필수 항목입니다.")
     private LocalDate scheduleDate;
 
-    @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$", message = "올바른 시간 형식이 아닙니다.")
+    @DateTimeFormat(pattern = "HH:mm[:ss]")
     @NotNull(message = "시작 시간은 필수 항목입니다.")
     private LocalTime startTime;
 
