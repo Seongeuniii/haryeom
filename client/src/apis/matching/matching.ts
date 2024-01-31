942e1084f3b87922f735d0cd0651110e6500d5eb
김성은 <kse8538@naver.com>
2024-01-31 20:21:38 +0900

    Feat : ✨ 매칭 선생님 리스트 조회 api 인터페이스

--- client/src/apis/matching/matching.ts
+++ client/src/apis/matching/matching.ts
@@ -1,59 +1,12 @@
-import axios from 'axios';
-import { ISubject } from '@/apis/tutoring/tutoring';
+import { ISubject } from '../tutoring/tutoring';
 
-const path = '/matching';
-
-export interface IRequestMatching {
-    chatRoomId: number;
-    subjectId: number;
-    hourlyRate: number;
+export interface IOpenTeacher {
+    teacherId: number;
+    profileUrl: string;
+    name: string;
+    college: string;
+    career: number;
+    gender: string;
+    salary: number;
+    subjects: ISubject[];
 }
-
-export interface IResponseMatching {
-    matchingId: string;
-    isAccepted: boolean;
-}
-
-export interface IRequestMatchingStatus {
-    matchingId: string;
-    receiveMemberId: number;
-    senderName: string;
-    subject: ISubject;
-    hourlyRate: number;
-}
-
-export interface IResponseMatchingStatus {
-    recipientMemberId: number;
-    isAccepted: boolean;
-    teacherName: string;
-    studentName: string;
-    subject: ISubject;
-    hourlyRate: number;
-}
-
-export const requestMatching = async (requestMatching: IRequestMatching) => {
-    try {
-        const res = await axios.post(
-            `${process.env.NEXT_PUBLIC_API_SERVER}${path}/request`,
-            JSON.stringify(requestMatching),
-            { headers: { 'Content-Type': `application/json` } }
-        );
-        console.log(res.headers.location);
-        return res.headers.location;
-    } catch (e) {
-        return null;
-    }
-};
-
-export const responseMatching = async (responsetMatching: IResponseMatching) => {
-    try {
-        const res = await axios.post(
-            `${process.env.NEXT_PUBLIC_API_SERVER}${path}/response`,
-            responsetMatching
-        );
-        console.log(res.headers.location);
-        return res.headers.location;
-    } catch (e) {
-        return null;
-    }
-};
