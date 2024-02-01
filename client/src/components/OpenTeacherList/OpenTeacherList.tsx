fb6cd82add5ca5648f86ee314176d382873d0848
kse8538 <kse8538@naver.com>
2024-02-01 14:15:45 +0900

    Feat : ✨ 선생님 상세조회 페이지 문의하기 클릭시 채팅 세션 시작

--- client/src/components/OpenTeacherList/OpenTeacherList.tsx
+++ client/src/components/OpenTeacherList/OpenTeacherList.tsx
@@ -5,18 +5,47 @@ import { useModal } from '@/hooks/useModal';
 import OpenTeacherIntroduce from '@/components/OpenTeacherIntroduce';
 import { IOpenTeacher } from '@/apis/matching/matching';
 import { useEffect, useState } from 'react';
+import { useGetOpenTeacherDetail } from '@/queries/useGetOpenTeacherDetail';
+import { useRecoilState } from 'recoil';
+import chatSessionAtom from '@/recoil/atoms/chat';
+import { createChatRoom } from '@/apis/matching/create-chat-room';
 
 interface OpenTeacherListProps {
     openTeacherList: IOpenTeacher[];
 }
 
 const OpenTeacherList = ({ openTeacherList }: OpenTeacherListProps) => {
+    const [chatSession, setChatSession] = useRecoilState(chatSessionAtom);
     const { open, openModal, closeModal } = useModal();
+    const [selectedTeacherId, setSelectedTeacherId] = useState<number | null>(null);
+    const { data: openTeacherDetail } = useGetOpenTeacherDetail(selectedTeacherId);
+
+    /**
+     * TODO : chatSession 시작하는 custom hook 만들기 - ChatContainer에도 똑같은 코드 있음.
+     */
+    const openChatContainer = () => {
+        setChatSession((prev) => {
+            return { ...prev, openChat: true };
+        });
+    };
+
+    const joinChatRoom = (roomId: number) => {
+        setChatSession((prev) => {
+            return { ...prev, chatRoomId: roomId };
+        });
+    };
+
+    const startChat = async () => {
+        const chatRoomId = await createChatRoom(selectedTeacherId as number);
+        if (!chatRoomId) return;
+        openChatContainer();
+        joinChatRoom(chatRoomId);
+    };
 
     return (
         <>
             <Modal open={open} closeModal={closeModal}>
-                <OpenTeacherIntroduce />
+                <OpenTeacherIntroduce openTeacherDetail={openTeacherDetail} startChat={startChat} />
             </Modal>
             <StyledOpenTeacherList>
                 {openTeacherList ? (
@@ -24,7 +53,10 @@ const OpenTeacherList = ({ openTeacherList }: OpenTeacherListProps) => {
                         return (
                             <OpenTeacherCard
                                 openTeacher={openTeacher}
-                                onClick={() => openModal()}
+                                onClick={() => {
+                                    openModal();
+                                    setSelectedTeacherId(openTeacher.teacherId);
+                                }}
                                 key={`open_teacher_${index}`}
                             />
                         );
