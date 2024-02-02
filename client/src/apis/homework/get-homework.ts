528f7d02ba260e810229a142da106f3d2820c27f
김성은 <kse8538@naver.com>
2024-02-02 10:01:36 +0900

    Hotfix : 숙제 드로잉 더미 연결

--- client/src/apis/homework/get-homework.ts
+++ client/src/apis/homework/get-homework.ts
@@ -6,35 +6,25 @@ const path = '/homework';
 type ReturnType = IHomework;
 
 export const getHomework = async (homeworkId: string) => {
-    try {
-        const res = await axios.get<ReturnType>(
-            `${process.env.NEXT_PUBLIC_API_SERVER}${path}/${homeworkId}`
-        );
-        return res.data;
-    } catch (e) {
-        console.log(e);
-    }
-    // return {
-    //     homeworkId: 1,
-    //     textbook: {
-    //         textbookId: 1,
-    //         textbookName: '자료명',
-    //         textbookURL: 'http://www.e-ffyc.re.kr/xml/03711/03711.pdf',
-    //         totalPage: 250,
-    //     },
-    //     startPage: 2,
-    //     endPage: 6,
-    //     drawings: [
-    //         {
-    //             drawingId: 1,
-    //             page: 3,
-    //             homeworkDrawingURL: '',
-    //         },
-    //         {
-    //             drawingId: 2,
-    //             page: 5,
-    //             homeworkDrawingURL: '',
-    //         },
-    //     ],
-    // };
+    // try {
+    //     const res = await axios.get<ReturnType>(
+    //         `${process.env.NEXT_PUBLIC_API_SERVER}${path}/${homeworkId}`
+    //     );
+    //     return res.data;
+    // } catch (e) {
+    //     console.log(e);
+    // }
+    return {
+        homeworkId: 1,
+        textbook: {
+            textbookId: 1,
+            textbookName: '수능특강 수학',
+            textbookUrl:
+                'https://d1b632bso7m0wd.cloudfront.net/EBS_2024%ED%95%99%EB%85%84%EB%8F%84_%EC%88%98%EB%8A%A5%ED%8A%B9%EA%B0%95_%EC%88%98%ED%95%99%EC%98%81%EC%97%AD_%EC%88%98%ED%95%99%E2%85%A0.pdf',
+            totalPage: 250,
+        },
+        startPage: 2,
+        endPage: 6,
+        drawings: [],
+    };
 };
