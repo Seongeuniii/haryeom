9181a73d90a47ce7115efddcfe3250c70058fba4
이상영 <syxxng25@gmail.com>
2024-02-02 17:32:28 +0900

    Feat : ✨ RedisConfig 직렬화 설정 (#S10P12A807-255)

--- server/src/main/java/com/ioi/haryeom/config/RedisConfig.java
+++ server/src/main/java/com/ioi/haryeom/config/RedisConfig.java
@@ -1,8 +1,5 @@
 package com.ioi.haryeom.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -50,23 +47,9 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
-        // key 직렬화 방식 설정
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-
-        // Jackson2JsonRedisSerializer 설정
-        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-            ObjectMapper.DefaultTyping.NON_FINAL,
-            JsonTypeInfo.As.PROPERTY);
-        serializer.setObjectMapper(objectMapper);
-
-        // Value 직렬화 방식 설정
-        redisTemplate.setValueSerializer(serializer);
-
-        redisTemplate.afterPropertiesSet();
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
     }
-
 }
