3b1276db96ec582db2877a1574804d11371e0a8b
이상영 <syxxng25@gmail.com>
2024-02-02 10:40:15 +0900

    Chore : 🧹Response Dto 생성자

--- server/src/main/java/com/ioi/haryeom/matching/dto/RespondToMatchingResponse.java
+++ server/src/main/java/com/ioi/haryeom/matching/dto/RespondToMatchingResponse.java
@@ -4,17 +4,23 @@ import com.ioi.haryeom.chat.domain.ChatRoom;
 import com.ioi.haryeom.common.domain.Subject;
 import com.ioi.haryeom.common.dto.SubjectResponse;
 import com.ioi.haryeom.member.domain.Member;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
 @Getter
 public class RespondToMatchingResponse {
 
-    private final Long recipientMemberId;
-    private final Boolean isAccepted;
-    private final String teacherName;
-    private final String studentName;
-    private final SubjectResponse subject;
-    private final Integer hourlyRate;
+    private Long recipientMemberId;
+    private Boolean isAccepted;
+    private String teacherName;
+    private String studentName;
+    private SubjectResponse subject;
+    private Integer hourlyRate;
 
 
     private RespondToMatchingResponse(Long recipientMemberId, String teacherName, String studentName, Boolean isAccepted, Subject subject,
