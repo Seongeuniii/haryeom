908dfa252fdad16664fce286b2df70482eaecb4d
김성은 <kse8538@naver.com>
2024-02-04 01:44:12 +0900

    Feat : ✨ 학습자료 없음

--- client/src/components/CreateNewHomework/CreateNewHomework.tsx
+++ client/src/components/CreateNewHomework/CreateNewHomework.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import { useModal } from '@/hooks/useModal';
 import Modal from '@/components/commons/Modal';
-import CreateNewHomeworkForm from './CreateNewHomeworkForm';
 import { ChangeEvent, useState } from 'react';
 import { registHomework } from '@/apis/homework/regist-homework';
+import InputForm from '@/components/commons/InputForm';
+import { ITutoringTextbook } from '@/apis/tutoring/tutoring';
+import Link from 'next/link';
 
 export interface INewHomework {
     [key: string]: string | number;
@@ -13,7 +15,11 @@ export interface INewHomework {
     endPage: number;
 }
 
-const CreateNewHomework = () => {
+interface CreateNewHomeworkProps {
+    tutoringTextbooks: ITutoringTextbook[];
+}
+
+const CreateNewHomework = ({ tutoringTextbooks }: CreateNewHomeworkProps) => {
     const { open, openModal, closeModal } = useModal();
     const [homeworkData, setHomeworkData] = useState<INewHomework>({
         textbookId: 0,
@@ -35,10 +41,44 @@ const CreateNewHomework = () => {
         else alert('등록에 실패했어요:)');
     };
 
+    console.log(tutoringTextbooks);
+
     return (
         <>
             <Modal open={open} closeModal={closeModal}>
-                <CreateNewHomeworkForm handleChange={handleChange} registForm={registForm} />
+                <StyledCreateNewHomeworkForm>
+                    <CreateNewHomeworkFormHeader>학생 숙제 등록</CreateNewHomeworkFormHeader>
+                    {!tutoringTextbooks ? (
+                        <NoContents>
+                            <span>학습자료가 없어요.</span>
+                            <Link href="/mycontents">학습자료 등록하러 가기</Link>
+                        </NoContents>
+                    ) : (
+                        <>
+                            <InputForm
+                                label={'마감일자'}
+                                name={'deadline'}
+                                handleChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
+                            />
+                            <InputForm
+                                label={'학습자료 선택'}
+                                name={'textbookId'}
+                                handleChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
+                            />
+                            <InputForm
+                                label={'시작페이지'}
+                                name={'startPage'}
+                                handleChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
+                            />
+                            <InputForm
+                                label={'끝페이지'}
+                                name={'endPage'}
+                                handleChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
+                            />
+                            <SubmitButton onClick={registForm}>등록</SubmitButton>
+                        </>
+                    )}
+                </StyledCreateNewHomeworkForm>
             </Modal>
             <StyledCreateNewHomework>
                 <OpenModalButton onClick={openModal}>+</OpenModalButton>
@@ -51,6 +91,54 @@ const StyledCreateNewHomework = styled.div`
     bottom: 1.5em;
     left: 0;
     text-align: center;
+`;
+
+const StyledCreateNewHomeworkForm = styled.div`
+    min-width: 500px;
+    padding: 2em;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    text-align: start;
+    border-radius: 1em;
+`;
+
+const CreateNewHomeworkFormHeader = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 0.8em;
+`;
+
+const NoContents = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
+    font-size: 14px;
+
+    a {
+        text-decoration: underline;
+        color: ${({ theme }) => theme.PRIMARY};
+        font-size: 16px;
+    }
+`;
+
+const SubmitButton = styled.button`
+    width: 100%;
+    height: 35px;
+    margin-top: 1em;
+    background-color: ${({ theme }) => theme.PRIMARY_LIGHT};
+    color: white;
+    border-radius: 0.5em;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.PRIMARY};
+    }
 `;
 
 const OpenModalButton = styled.button`
