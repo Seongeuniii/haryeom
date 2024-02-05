6e3f70d69e6f6651bbfb698ebf9ad0ec87163d58
이상영 <syxxng25@gmail.com>
2024-02-05 17:50:25 +0900

    Hotfix : 🚑 Redis HashOperations import

--- server/src/main/java/com/ioi/haryeom/auth/service/AuthService.java
+++ server/src/main/java/com/ioi/haryeom/auth/service/AuthService.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
