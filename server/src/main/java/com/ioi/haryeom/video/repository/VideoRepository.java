a4dd6f4522ad50fab70927e36211f25ce1659ee1
김태윤 <whddn_0426@naver.com>
2024-02-06 17:59:47 +0900

    Refactor : 🔨 영상 녹화 api 변경에 따른 코드 수정 (#S10P12A807-129)

--- server/src/main/java/com/ioi/haryeom/video/repository/VideoRepository.java
+++ server/src/main/java/com/ioi/haryeom/video/repository/VideoRepository.java
@@ -1,11 +1,13 @@
 package com.ioi.haryeom.video.repository;
 
 import com.ioi.haryeom.video.domain.Video;
+import java.util.Optional;
+import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
-    Video findByTutoringSchedule_Id(Long tutoringScheduleId);
+    Optional<Video> findByTutoringSchedule_Id(Long tutoringScheduleId);
 
 }
