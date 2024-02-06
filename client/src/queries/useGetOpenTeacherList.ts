0f29f9318d53cea67167f48eb751b5b02fd3709a
김성은 <kse8538@naver.com>
2024-02-06 20:33:16 +0900

    Hotfix : 빌드에러

--- client/src/queries/useGetOpenTeacherList.ts
+++ client/src/queries/useGetOpenTeacherList.ts
@@ -1,15 +1,14 @@
 import { useQuery } from 'react-query';
-import { ITutoringSchedules } from '@/apis/tutoring/tutoring';
 import { getOpenTeacherList } from '@/apis/matching/get-open-teacher-list';
 import { IOpenTeacher } from '@/apis/matching/matching';
 
 export const useGetOpenTeacherList = (
-    filteres: { [key: string]: string[] | number },
+    filterers: { [key: string]: string[] | number },
     initialData?: IOpenTeacher[]
 ) => {
     const { data, isLoading } = useQuery({
-        queryKey: ['getOpenTeacherList', filteres],
-        queryFn: () => getOpenTeacherList(filteres),
+        queryKey: ['getOpenTeacherList', filterers],
+        queryFn: () => getOpenTeacherList(filterers),
         cacheTime: Infinity,
         initialData: initialData,
     });
