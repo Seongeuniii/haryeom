5ab78b6dadfaf645056e1cc0b9df72d0574dbbb1
kse8538 <kse8538@naver.com>
2024-02-08 10:12:27 +0900

    Hotfix : 🚑 테스트용 페이지 라우팅 에러

--- client/src/components/LoginModal.tsx
+++ client/src/components/LoginModal.tsx
@@ -9,7 +9,7 @@ const LoginModal = () => {
             <StyledLoginModal>
                 <LoginModalHeader>하렴 서비스를 이용하려면 로그인이 필요해요</LoginModalHeader>
                 <ServiceImg />
-                <Link href="/demologin">2월 8일 테스트를 위한 계정입니다.</Link>
+                <Link href="/logindemo">2월 8일 테스트를 위한 계정입니다.</Link>
                 <Link href="/login">로그인 하러가기</Link>
                 <Link href="/find">선생님 둘러보기</Link>
             </StyledLoginModal>
