ccd45ff6799ee7003cedc5ff41492c6064e9b8bb
김태윤 <whddn_0426@naver.com>
2024-02-12 08:32:30 +0900

    Refactor : 🔨 타임스탬프 코드 수정 (#S10P12A807-310)

---
+++ server/src/main/java/com/ioi/haryeom/video/exception/VideoTutoringNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ioi.haryeom.video.exception;
+
+import com.ioi.haryeom.advice.exception.NotFoundException;
+
+public class VideoTutoringNotFoundException extends NotFoundException {
+
+    private static final String MESSAGE = "해당하는 과외 일정에 맞는 영상이 존재하지 않습니다. 과외일정id: %d";
+    public VideoTutoringNotFoundException(Long tutoringScheduleId) {
+        super(String.format(MESSAGE, tutoringScheduleId));
+    }
+}
