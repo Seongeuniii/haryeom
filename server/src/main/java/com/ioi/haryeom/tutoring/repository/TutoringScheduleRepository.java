04202c035369dd69a6a055ab85b8b16d9c47e416
이상영 <syxxng25@gmail.com>
2024-02-16 01:43:12 +0900

    Feat : ✨ 월별 정산 조회 API 구현

--- server/src/main/java/com/ioi/haryeom/tutoring/repository/TutoringScheduleRepository.java
+++ server/src/main/java/com/ioi/haryeom/tutoring/repository/TutoringScheduleRepository.java
@@ -11,13 +11,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TutoringScheduleRepository extends JpaRepository<TutoringSchedule, Long>, TutoringScheduleRepositoryCustom {
+
     List<TutoringSchedule> findAllByScheduleDate(LocalDate scheduleDate);
 
     @Query(value = "SELECT * FROM tutoring_schedule ts " +
         "LEFT JOIN tutoring t ON ts.tutoring_id = t.id " +
         "LEFT JOIN member m ON t.teacher_member_id = m.id " +
         "WHERE m.id = :teacherMemberId " +
-        "AND DATE_ADD(STR_TO_DATE(CONCAT(ts.schedule_date, ' ', ts.start_time), '%Y-%m-%d %H:%i:%s'), INTERVAL ts.duration MINUTE) >= :startDateTime " +
+        "AND DATE_ADD(STR_TO_DATE(CONCAT(ts.schedule_date, ' ', ts.start_time), '%Y-%m-%d %H:%i:%s'), INTERVAL ts.duration MINUTE) >= :startDateTime "
+        +
         "AND STR_TO_DATE(CONCAT(ts.schedule_date, ' ', ts.start_time), '%Y-%m-%d %H:%i:%s') <= :endDateTime",
         nativeQuery = true)
     List<TutoringSchedule> findTutoringSchedulesByTeacherAndDateRange(
@@ -29,11 +31,19 @@ public interface TutoringScheduleRepository extends JpaRepository<TutoringSchedu
         "LEFT JOIN tutoring t ON ts.tutoring_id = t.id " +
         "LEFT JOIN member m ON t.student_member_id = m.id " +
         "WHERE m.id = :studentMemberId " +
-        "AND DATE_ADD(STR_TO_DATE(CONCAT(ts.schedule_date, ' ', ts.start_time), '%Y-%m-%d %H:%i:%s'), INTERVAL ts.duration MINUTE) >= :startDateTime " +
+        "AND DATE_ADD(STR_TO_DATE(CONCAT(ts.schedule_date, ' ', ts.start_time), '%Y-%m-%d %H:%i:%s'), INTERVAL ts.duration MINUTE) >= :startDateTime "
+        +
         "AND STR_TO_DATE(CONCAT(ts.schedule_date, ' ', ts.start_time), '%Y-%m-%d %H:%i:%s') <= :endDateTime",
         nativeQuery = true)
     List<TutoringSchedule> findTutoringSchedulesByStudentAndDateRange(
         @Param("studentMemberId") Long studentMemberId,
         @Param("startDateTime") LocalDateTime startDateTime,
         @Param("endDateTime") LocalDateTime endDateTime);
+
+    @Query(value = "SELECT ts.schedule_date, ts.title, IFNULL((v.end_time - v.start_time) / 60, 0) AS progress_time " +
+        "FROM tutoring_schedule ts " +
+        "LEFT JOIN video v ON ts.id = v.tutoring_schedule_id " +
+        "WHERE ts.tutoring_id = ?1 AND DATE_FORMAT(ts.schedule_date, '%Y%m') = ?2 " +
+        "ORDER BY ts.schedule_date", nativeQuery = true)
+    List<Object[]> findScheduleProgressByTutoringIdAndMonthNative(Long tutoringId, String yearMonth);
 }
