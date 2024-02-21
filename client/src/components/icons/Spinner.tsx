5e9b8534e2d824f7632880bf9cde370aba4ee387
김성은 <kse8538@naver.com>
2024-02-21 16:51:58 +0900

    Feat : ✨ loading spinner UI

---
+++ client/src/components/icons/Spinner.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+const Spinner = () => {
+    return (
+        <StyledSpinner>
+            <svg className="spinner" viewBox="0 0 50 50">
+                <circle
+                    className="path"
+                    cx="25"
+                    cy="25"
+                    r="20"
+                    fill="none"
+                    strokeWidth="5"
+                ></circle>
+            </svg>
+        </StyledSpinner>
+    );
+};
+
+const StyledSpinner = styled.div`
+    .spinner {
+        animation: rotate 2s linear infinite;
+        z-index: 2;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin: -25px 0 0 -25px;
+        width: 50px;
+        height: 50px;
+    }
+    .spinner .path {
+        stroke: ${({ theme }) => theme.PRIMARY};
+        stroke-linecap: round;
+        animation: dash 1.5s ease-in-out infinite;
+    }
+
+    @keyframes rotate {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes dash {
+        0% {
+            stroke-dasharray: 1, 150;
+            stroke-dashoffset: 0;
+        }
+        50% {
+            stroke-dasharray: 90, 150;
+            stroke-dashoffset: -35;
+        }
+        100% {
+            stroke-dasharray: 90, 150;
+            stroke-dashoffset: -124;
+        }
+    }
+`;
+
+export default Spinner;
