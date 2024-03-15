0788e166d2b0c3dc163faa2f6581e2e90eca682d
Seongeuniii <kse8538@naver.com>
2024-03-15 14:48:23 +0900

    Refactor : 🔨 전체 컴포넌트 리렌더링 유발하는 layout 제거 (#13)

--- client/src/containers/HomeworkContainer/HomeworkContainer.tsx
+++ client/src/containers/HomeworkContainer/HomeworkContainer.tsx
@@ -1,21 +1,23 @@
 import { GetServerSideProps } from 'next';
-import { useMemo, useRef, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
+import { QueryClient } from 'react-query';
 import PdfViewer from '@/components/PdfViewer';
 import PaintCanvas from '@/components/PaintCanvas';
-import HomeworkLayout from '@/components/layouts/HomeworkLayout';
 import { getHomework } from '@/apis/homework/get-homework';
 import { IHomework } from '@/apis/homework/homework';
-import usePdf, { IPdfSize } from '@/hooks/usePdf';
+import usePdf from '@/hooks/usePdf';
 import useMyPaint from '@/components/PaintCanvas/hooks/useMyPaint';
 import HomeworkStatus from '@/components/HomeworkStatus';
 import { IPenStyle } from '@/hooks/useClass';
 import { saveHomework, submitHomework } from '@/apis/homework/save-homework';
 import { useGetHomework } from '@/queries/useGetHomework';
-import { QueryClient } from 'react-query';
 import DrawingTools from '@/components/DrawingTools';
 import { useRouter } from 'next/router';
 import useCanvas from '@/components/PaintCanvas/hooks/useCanvas';
+import PdfThumbnailList from '@/components/PdfThumbnailList';
+import ChatContainer from '@/containers/ChatContainer';
+import HomeworkPageHeader from '@/components/HomeworkPageHeader';
 
 interface HomeworkContainerProps {
     homeworkData: IHomework;
@@ -31,7 +33,6 @@ export interface IHomeworkStatus {
 
 const HomeworkContainer = ({ homeworkData: initialHomeworkData }: HomeworkContainerProps) => {
     const router = useRouter();
-
     const { data: homeworkData, refetch } = useGetHomework(
         initialHomeworkData.homeworkId,
         initialHomeworkData
@@ -119,60 +120,75 @@ const HomeworkContainer = ({ homeworkData: initialHomeworkData }: HomeworkContai
         }, {} as IHomeworkStatus);
     }, [homeworkData, myHomeworkDrawings]);
 
+    const handleSave = useCallback(async () => {
+        await saveHomework(homeworkData.homeworkId, myHomeworkDrawings);
+        refetch();
+        alert('숙제가 임시 저장되었어요. 제출하여 완료해주세요:)');
+    }, [homeworkData, myHomeworkDrawings]);
+
+    const handleSubmit = useCallback(async () => {
+        await saveHomework(homeworkData.homeworkId, myHomeworkDrawings);
+        await submitHomework(homeworkData.homeworkId);
+        alert('숙제가 제출되었어요. 홈 화면으로 이동합니다:)');
+        router.push('/');
+    }, [homeworkData, myHomeworkDrawings]);
+
     return (
-        <HomeworkLayout
-            homeworkData={homeworkData}
-            handleSave={async () => {
-                await saveHomework(homeworkData.homeworkId, myHomeworkDrawings);
-                refetch();
-                alert('숙제가 임시 저장되었어요. 제출하여 완료해주세요:)');
-            }}
-            handleSubmit={async () => {
-                await saveHomework(homeworkData.homeworkId, myHomeworkDrawings);
-                await submitHomework(homeworkData.homeworkId);
-                alert('숙제가 제출되었어요. 홈 화면으로 이동합니다:)');
-                router.push('/');
-            }}
-        >
-            <StyledHomeworkContainer>
-                <Board>
-                    <DrawingToolWrapper>
-                        <DrawingTools
-                            penStyle={penStyle}
-                            changePenStyle={changePenStyle}
-                            resetCanvas={resetCanvas}
-                        />
-                    </DrawingToolWrapper>
-                    <PdfViewer
-                        pdfFile={homeworkData.textbook.textbookUrl}
-                        selectedPageNumber={selectedPageNumber}
-                        totalPagesOfPdfFile={totalPagesOfPdfFile}
-                        pdfPageCurrentSize={pdfPageCurrentSize}
-                        movePage={movePage}
-                        onDocumentLoadSuccess={onDocumentLoadSuccess}
-                        onPageLoadSuccess={onPageLoadSuccess}
-                        updatePdfPageCurrentSize={updatePdfPageCurrentSize}
-                        ZoomInPdfPageCurrentSize={ZoomInPdfPageCurrentSize}
-                        ZoomOutPdfPageCurrentSize={ZoomOutPdfPageCurrentSize}
-                        myHomeworkDrawings={myHomeworkDrawings}
-                        startPageNumber={homeworkData.startPage}
-                    >
-                        <DrawingLayer>
-                            <PaintCanvas
-                                canvasRef={homeworkCanvasRef}
-                                handlePointerDown={handlePointerDown}
-                                handlePointerMove={handlePointerMove}
-                                handlePointerUp={() => {
-                                    handlePointerUp();
-                                    saveHomeworkDrawing();
-                                }}
+        <StyledHomeworkContainer>
+            <HomeworkPageHeader
+                homeworkData={homeworkData}
+                handleSave={handleSave}
+                handleSubmit={handleSubmit}
+            />
+            <ContainerWrapper>
+                <BoardWrapper>
+                    <Board>
+                        <DrawingToolWrapper>
+                            <DrawingTools
+                                penStyle={penStyle}
+                                changePenStyle={changePenStyle}
+                                resetCanvas={resetCanvas}
                             />
-                        </DrawingLayer>
-                    </PdfViewer>
-                </Board>
-                <HomeworkStatus homeworkData={homeworkData} homeworkStatus={homeworkStatus} />
-            </StyledHomeworkContainer>
-        </HomeworkLayout>
+                        </DrawingToolWrapper>
+                        <PdfThumbnailList
+                            pdfFile={homeworkData.textbook.textbookUrl}
+                            totalPagesOfPdfFile={totalPagesOfPdfFile}
+                            startPageNumber={homeworkData.startPage}
+                            selectedPageNumber={selectedPageNumber}
+                            movePage={movePage}
+                        />
+                        <PdfViewer
+                            pdfFile={homeworkData.textbook.textbookUrl}
+                            selectedPageNumber={selectedPageNumber}
+                            totalPagesOfPdfFile={totalPagesOfPdfFile}
+                            pdfPageCurrentSize={pdfPageCurrentSize}
+                            movePage={movePage}
+                            onDocumentLoadSuccess={onDocumentLoadSuccess}
+                            onPageLoadSuccess={onPageLoadSuccess}
+                            updatePdfPageCurrentSize={updatePdfPageCurrentSize}
+                            ZoomInPdfPageCurrentSize={ZoomInPdfPageCurrentSize}
+                            ZoomOutPdfPageCurrentSize={ZoomOutPdfPageCurrentSize}
+                            myHomeworkDrawings={myHomeworkDrawings}
+                            startPageNumber={homeworkData.startPage}
+                        >
+                            <DrawingLayer>
+                                <PaintCanvas
+                                    canvasRef={homeworkCanvasRef}
+                                    handlePointerDown={handlePointerDown}
+                                    handlePointerMove={handlePointerMove}
+                                    handlePointerUp={() => {
+                                        handlePointerUp();
+                                        saveHomeworkDrawing();
+                                    }}
+                                />
+                            </DrawingLayer>
+                        </PdfViewer>
+                    </Board>
+                    <HomeworkStatus homeworkData={homeworkData} homeworkStatus={homeworkStatus} />
+                </BoardWrapper>
+            </ContainerWrapper>
+            <ChatContainer />
+        </StyledHomeworkContainer>
     );
 };
 
@@ -193,6 +209,39 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 const StyledHomeworkContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: end;
+`;
+
+const SubmitButtons = styled.div`
+    position: absolute;
+    right: 16.5%;
+    display: flex;
+    gap: 7px;
+
+    @media screen and (max-width: 1200px) {
+        & {
+            right: 2%;
+        }
+    }
+`;
+
+const SubmitHomeworkButton = styled.button`
+    padding: 5px 12px;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.PRIMARY_LIGHT};
+    color: white;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.PRIMARY};
+        transition: all 0.5s;
+    }
+`;
+
+const BoardWrapper = styled.div`
     background-color: white;
     width: 100%;
     height: 100%;
@@ -230,6 +279,18 @@ const DrawingToolWrapper = styled.div`
     display: flex;
     gap: 10px;
     z-index: 10;
+`;
+
+const ContainerWrapper = styled.main`
+    width: 70%;
+    height: calc(100% - 3em);
+    display: flex;
+
+    @media screen and (max-width: 1200px) {
+        & {
+            width: 100%;
+        }
+    }
 `;
 
 export default HomeworkContainer;
